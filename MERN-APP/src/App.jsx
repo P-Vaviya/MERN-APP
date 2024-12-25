dfbc24434d0191333d3adbb3caad81d402be7f7d
Prince Vaviya <1princevaviya@gmail.com>
2024-12-25 21:47:28 +0530

    Header Components

--- MERN-APP/src/App.jsx
+++ MERN-APP/src/App.jsx
@@ -5,10 +5,12 @@ import Dashboard from './Pages/Dashboard'
 import Contact from './Pages/Contact'
 import SignIn from './Pages/SignIn'
 import SignUp from './Pages/SignUp'
+import Header from './Header/Header'
 
 export default function App(){
   return(
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/about' element={<About />} />
